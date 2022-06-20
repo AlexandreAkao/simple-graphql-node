348a8b8ac336142e702519596217eca473c1ccbe
Alexandre Akira <alexandreakiraakao@gmail.com>
2022-06-20 15:45:08 -0300

    add createdAt and updatedAt on schemas

--- src/typeDefs/types.js
+++ src/typeDefs/types.js
@@ -1,21 +1,29 @@
 const { gql } = require("apollo-server");
 
 const types = gql`
+  scalar DateTime
+
   type User {
     id: ID!
     name: String
     age: Int
+    createdAt: DateTime
+    updatedAt: DateTime
   }
   type Music {
     id: ID!
     name: String
     artist: String
     playlistIds: [ID!]
+    createdAt: DateTime
+    updatedAt: DateTime
   }
   type Playlist {
     id: ID!
     name: String
     userId: ID!
+    createdAt: DateTime
+    updatedAt: DateTime
   }
 `;
 
